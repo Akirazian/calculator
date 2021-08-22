a5730746312fbe24c7f3c5631e8d6432adf8bc35
Akiraz <alec.kirazian@gmail.com>
2021-08-22 16:17:09 -0400

    Added keyboard support

--- calculator.js
+++ calculator.js
@@ -49,15 +49,29 @@ let currentOperation = "";
 let writeOverCheck = true;
 
 function numberPress(event) {
-  if (writeOverCheck === true) { 
-    if (event.target.innerText === "0") return; //prevent multiple 0's 
-    display.innerText = event.target.innerText;
-    writeOverCheck = false;
-    return;
-  } 
+  if (event.type === "click") { 
+    if (writeOverCheck === true) { 
+      if (event.target.innerText === "0") return; //prevent multiple 0's 
+      display.innerText = event.target.innerText;
+      writeOverCheck = false;
+      return;
+    } 
 
-  if (display.innerText.includes(".") && event.target.innerText === ".") return; //prevent more than one "."
-  display.innerText += event.target.innerText;
+    if (display.innerText.includes(".") && event.target.innerText === ".") return; //prevent more than one "."
+    display.innerText += event.target.innerText;
+  }
+
+  if (event.type === "keydown") { 
+    if (writeOverCheck === true) { 
+      if (event.key === "0") return; //prevent multiple 0's 
+      display.innerText = event.key;
+      writeOverCheck = false;
+      return;
+    } 
+    
+    if (display.innerText.includes(".") && event.key === ".") return; //prevent more than one "."
+    display.innerText += event.key;
+  }
 }
 
 function clearAll() {
@@ -74,18 +88,30 @@ function clearCurrent() {
 }
 
 function operatorPress(event) {
-  if (firstNumber != 0 && firstNumber != display.innerText) { //Allows strining together multiple operators
-    firstNumber = getAnswer();
-    display.innerText = firstNumber;
-  };
+    if (firstNumber != 0 && firstNumber != display.innerText) { //Allows strining together multiple operators
+      firstNumber = getAnswer();
+      display.innerText = firstNumber;
+    };
 
-  firstNumber = +display.innerText;
-  currentOperation = event.target.innerText;
-  if (event.target.id === "exponent" || event.target.tagName === "SUP") {
-    currentOperation = "^"
+  if (event.type === "click") {
+    firstNumber = +display.innerText;
+    currentOperation = event.target.innerText;
+    if (event.target.id === "exponent" || event.target.tagName === "SUP") {
+      currentOperation = "^"
+    }
+    writeOverCheck = true;
+    operationDisplay.innerText = ` ${firstNumber} ${currentOperation}`;
   }
-  writeOverCheck = true;
-  operationDisplay.innerText = ` ${firstNumber} ${currentOperation}`;
+
+  if (event.type === "keydown") {
+    firstNumber = +display.innerText;
+    currentOperation = event.key;
+    if (event.key === "-") currentOperation = "−";
+    if (event.key === "*") currentOperation = "×";
+    if (event.key === "/") currentOperation = "÷";
+    writeOverCheck = true;
+    operationDisplay.innerText = ` ${firstNumber} ${currentOperation}`;
+  }
 }
 
 function clearCurrent() {
@@ -125,3 +151,15 @@ clearAllButton.addEventListener("click", clearAll);
 deleteButton.addEventListener("click", clearCurrent);
 equalsButton.addEventListener("click", equalsPress);
 plusMinusButton.addEventListener("click", flipSign);
+document.addEventListener("keydown", (event) => {
+  if (+event.key >= 0 || event.key === ".") {
+    numberPress(event);
+  }
+  let operators = ["+", "-", "*", "/", "^"];
+  if (operators.indexOf(event.key) > -1) {
+    operatorPress(event);
+  }
+  if (event.key === "Enter") equalsPress();
+  if (event.key === "Backspace") display.innerText = display.innerText.slice(0, -1);
+  if (event.key === "Delete") clearAll();
+});
